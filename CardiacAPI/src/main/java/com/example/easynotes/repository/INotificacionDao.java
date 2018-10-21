2b9ac64333f5fbbf87d8d7d5e87dcd2b37e0e0e6
miguelb10 <miguel.blas_03@hotmail.com>
2018-10-21 02:19:44 -0500

    Commit 2
    Correccion

--- CardiacAPI/src/main/java/com/example/easynotes/repository/INotificacionDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.easynotes.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.easynotes.model.Notificacion;
-
-public interface INotificacionDao extends CrudRepository<Notificacion, Integer>{
-
-}
